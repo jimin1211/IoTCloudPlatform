8ca86ec10e2264d5da592cb7f91ef4857c6bb675
Areum-lab <80264647+Areum-lab@users.noreply.github.com>
2021-12-03 13:31:27 +0900

    Android

---
+++ app/src/main/java/com/example/android_resapi/ui/DeviceActivity.java
@@ -0,0 +1,128 @@
+package com.example.android_resapi.ui;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.example.android_resapi.R;
+import com.example.android_resapi.ui.apicall.GetThingShadow;
+import com.example.android_resapi.ui.apicall.UpdateShadow;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class DeviceActivity extends AppCompatActivity {
+    String urlStr;
+    final static String TAG = "AndroidAPITest";
+    Timer timer;
+    Button startGetBtn;
+    Button stopGetBtn;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_device);
+        Intent intent = getIntent();
+        urlStr = intent.getStringExtra("thingShadowURL");
+
+        startGetBtn = findViewById(R.id.startGetBtn);
+        startGetBtn.setEnabled(true);
+        startGetBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                timer = new Timer();
+                timer.schedule(new TimerTask() {
+                    @Override
+                    public void run() {
+                        new GetThingShadow(DeviceActivity.this, urlStr).execute();
+                    }
+                },
+                        0,2000);
+
+                startGetBtn.setEnabled(false);
+                stopGetBtn.setEnabled(true);
+            }
+        });
+
+        stopGetBtn = findViewById(R.id.stopGetBtn);
+        stopGetBtn.setEnabled(false);
+        stopGetBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (timer != null)
+                    timer.cancel();
+                clearTextView();
+                startGetBtn.setEnabled(true);
+                stopGetBtn.setEnabled(false);
+            }
+        });
+
+        Button updateBtn = findViewById(R.id.updateBtn);
+        updateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText edit_dist = findViewById(R.id.edit_dist);
+                EditText edit_led = findViewById(R.id.edit_led);
+
+                JSONObject payload = new JSONObject();
+
+                try {
+                    JSONArray jsonArray = new JSONArray();
+                    String dist_input = edit_dist.getText().toString();
+                    if (dist_input != null && !dist_input.equals("")) {
+                        JSONObject tag1 = new JSONObject();
+                        tag1.put("tagName", "distance");
+                        tag1.put("tagValue", dist_input);
+
+                        jsonArray.put(tag1);
+                    }
+
+                    String led_input = edit_led.getText().toString();
+                    if (led_input != null && !led_input.equals("")) {
+                        JSONObject tag2 = new JSONObject();
+                        tag2.put("tagName", "LED");
+                        tag2.put("tagValue", led_input);
+
+                        jsonArray.put(tag2);
+                    }
+
+                    if (jsonArray.length() > 0)
+                        payload.put("tags", jsonArray);
+                } catch (JSONException e) {
+                    Log.e(TAG, "JSONEXception");
+                }
+                Log.i(TAG,"payload="+payload);
+                if (payload.length() >0 )
+                    new UpdateShadow(DeviceActivity.this,urlStr).execute(payload);
+                else
+                    Toast.makeText(DeviceActivity.this,"변경할 상태 정보 입력이 필요합니다", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+
+    private void clearTextView() {
+        TextView reported_ledTV = findViewById(R.id.reported_led);
+        TextView reported_distTV = findViewById(R.id.reported_dist);
+        reported_distTV.setText("");
+        reported_ledTV.setText("");
+
+        TextView desired_ledTV = findViewById(R.id.desired_led);
+        TextView desired_distTV = findViewById(R.id.desired_dist);
+        desired_distTV.setText("");
+        desired_ledTV.setText("");
+    }
+
+}
+
+
